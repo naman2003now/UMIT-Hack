6808e74c36ae2e4f15787128071f52e5cbfe9dd2
rbSparky <rishb369@gmail.com>
2022-02-07 18:18:13 +0530

    pls

--- new/test.py
+++ new/test.py
@@ -1,5 +1,5 @@
 import pickle
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, session
 from flask_cors import CORS, cross_origin
 import sklearn
 from sklearn.decomposition import TruncatedSVD
@@ -22,8 +22,12 @@ class Model:
   cmat = []
   fir = []
   sec = []
+  final = []
 
   def predict(self):
+    self.sec = []
+    self.fir = []
+    flf = []
     SVD = TruncatedSVD(n_components=10)
     decomposed_matrix = SVD.fit_transform(self.cmat)
     df=pd.DataFrame(decomposed_matrix)
@@ -42,23 +46,53 @@ class Model:
       if (fl[i][0] > 0.95):
         flf.append(fl[i])
 
+    clgdf, clgds = {}, {}
     for i in flf:
       for j in self.cmat.loc[i[1]].items():
-        if(j[1] == 5 and j[0] not in self.fir and (ranks[j[0]][1] > self.lrank)):
-          self.sec.append([(j[0].split())[0], j[0][len((j[0].split())[0]):], ranks[j[0]][0], ranks[j[0]][1]])
-        elif (j[1] == 2 and j[0] not in self.sec and (ranks[j[0]][1] > self.lrank)):
-          self.sec.append([(j[0].split())[0], j[0][len((j[0].split())[0]):], ranks[j[0]][0], ranks[j[0]][1]])
+        if ((j[1] == 5) and (j[0] not in self.fir) and (ranks[j[0]][1] > self.lrank)):
+          if(j[0] in clgdf):
+            clgdf[j[0]] += 1
+          else:
+            clgdf[j[0]] = 1
 
-    print(self.fir, self.sec, sep = '\n\n')
-    final = []
+          
+        elif ((j[1] == 2) and (j[0] not in self.sec) and (ranks[j[0]][1] > self.lrank)):
+          if(j[0] in clgds):
+            clgds[j[0]] += 1
+          else:
+              clgds[j[0]] = 1            
+
+    tf, ts = [], []
+    for k in clgdf:
+      tf.append([clgdf[k], k])
+    for k in clgds:
+      ts.append([clgds[k], k])
+
+    tf.sort(reverse=True)
+    ts.sort(reverse=True)
+
+    for i in tf:
+      j = i[1]
+      self.fir.append([(j.split())[0], j[len((j[0].split())[0]):], ranks[j][0], ranks[j][1]])
+
+    
+    for i in ts:
+      j = i[1]
+      self.sec.append([(j.split())[0], j[len((j[0].split())[0]):], ranks[j][0], ranks[j][1]])
+
+    print(self.fir, self.sec, sep = '\n\n\n')
+    self.final = []
     for i in self.fir:
-        final.append(i)
+        self.final.append(i)
     for i in self.sec:
-        final.append(i)
+        self.final.append(i)
+    rfinal = []
+    [rfinal.append(x) for x in self.final if x not in rfinal]
+
     finalstr = str()
-    for i in final:
+    for i in self.final:
         finalstr += str(i) + '\n'
-    return jsonify(final)
+    return jsonify(rfinal)
 
 @app.route('/')
 def hello():
@@ -69,6 +103,8 @@ def hello():
 def predict():
   #take all these as input from args
   global ranks
+
+  #session.clear()
   req_dat = request.get_json()
   lrank = req_dat['lrank']#5000
   hrank = req_dat['hrank']#7000
@@ -92,8 +128,13 @@ def predict():
   f2.hrank = hrank
   f2.stream1 = stream1
   f2.stream2 = stream2
+  f2.final = []
   f2.i = wsc[(stream1, stream2)]
+
   return f2.predict()
 
 if __name__ == '__main__':
+  #app.secret_key = 'super secret key'
+  #app.config['SESSION_TYPE'] = 'filesystem'
+  #session.init_app(app)
   app.run(debug = True)
